05fe4258d7ee3f7eb58a9c3b7de93a0a647d7ead
Hari Velayudam <Hari Velayudam@DESKTOP-JMGP3K7>
2020-08-02 01:31:58 -0700

    Two Sum accouting for repeat

--- Java_Basics/src/Main.java
+++ Java_Basics/src/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		TwoSum _instance = new TwoSum();
-		int nums[] = {2,2,2,2,2};
-		int[] result = _instance.findTwoSum(nums, 8);
+		int nums[] = {2,2,10};
+		int[] result = _instance.findTwoSum(nums, 12);
 		System.out.println("Result : "+result[0]+" and "+result[1]);
 		
 	}
--- Java_Basics/src/algos/implementations/arrays/TwoSum.java
+++ Java_Basics/src/algos/implementations/arrays/TwoSum.java
@@ -1,25 +1,32 @@
 package algos.implementations.arrays;
 
 public class TwoSum {
-	
-	public int[] findTwoSum(int[] nums, int sum) {
-		
+
+	public int[] findTwoSum(int[] nums, int target) {
 		int[] result = new int[2];
-		
-		for(int i=0; i<nums.length-1; i++) {
-			for(int j=i; j<=nums.length-1;j++) {
-			if((nums[i] + nums[j]) == sum) {
-				result[0] = i;
-				result[1] = j;
-				System.out.println(" In if");
-				break;
-			} else {
-				System.out.println("IN else");
+		boolean found = false;
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (!found) {
+				for (int j = i + 1; j <= nums.length - 1; j++) {
+					if ((nums[i] + nums[j]) == target) {
+						result[0] = i;
+						result[1] = j;
+						found = true;
+						break;
+					}
+				}
 			}
-			}
+
 		}
+		return result;
+	}
+	
+	public int[] findTwoSum_pointers(int []nums, int target ) {
+		
+		int result[] = new int[2];
 		
 		return result;
+		
 	}
 
 }
